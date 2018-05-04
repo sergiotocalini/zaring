30e5d5c6165fd3a96cf0f9abf9f03d49c4707cbc
root <root@re.test.03.amana.vpn>
2018-05-04 15:03:11 +0000

    Adding service items.

--- zaring/zaring.sh
+++ zaring/zaring.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env ksh
+IFS_DEFAULT="${IFS}"
 PATH=/usr/local/bin:${PATH}
 
 #################################################################################
@@ -62,38 +63,92 @@ refresh_cache() {
 }
 
 discovery() {
+    IFS="${IFS_DEFAULT}"
     resource=${1}
     json=$(refresh_cache)
     if [[ ${resource} == 'projects' ]]; then
-	for proj in /etc/spring-boot/conf.d/*.conf; do
-	    :
-	    set -- "${proj}"
-	    if [ -f ${proj} ]; then
-		. ${proj}
-	    fi
-	    
-	    echo "${APP_NAME}: ${APP_DESC}"
+	for configfile in $(get_configfile); do
+	    jq -r 'select(.monitoring.enable=="yes")|"\(.name)|\(.monitoring.jmx)|\(.version)|"' \
+	       ${configfile} 2>/dev/null
 	done
     fi
     return 0
 }
 
+get_configfile() {
+    resource=${1:-all}
+
+    JSON_DIR="/etc/spring-boot/conf.d"
+    if [[ ${resource} != 'all' ]]; then
+       for configfile in ${JSON_DIR}/*.json; do
+          name=`jq -r 'select(.name=="'${resource}'")|.name' ${configfile} 2>/dev/null`
+          if [[ ${name} == ${resource} ]]; then
+             res=${configfile}
+             break
+          fi
+       done
+    else
+       count=0
+       for configfile in ${JSON_DIR}/*.json; do
+          res[${count}]=${configfile}
+          let "count=count+1"
+       done
+    fi
+    echo "${res[@]:-0}"
+    return 0
+}
+
 get_service() {
     resource=${1}
+    property=${2:-listen}
 
-    port=`echo "${ZARING_URL}" | sed -e 's|.*://||g' -e 's|/||g' | awk -F: '{print $2}'`
-    pid=`sudo lsof -Pi :${port:-8080} -sTCP:LISTEN -t`
-    rcode="${?}"
-    if [[ ${resource} == 'listen' ]]; then
-	if [[ ${rcode} == 0 ]]; then
-	    res=1
+    json=$(get_configfile ${resource})
+    if [[ ${property} == 'listen' ]]; then
+        app_port=(`jq -r '.monitoring.port|@sh' ${json} 2>/dev/null`)
+        for index in ${!app_port[*]}; do
+           pid=`sudo lsof -Pi :${app_port[${index}]} -sTCP:LISTEN -t 2>/dev/null`
+           rcode="${?}"
+           if [[ ${rcode} == 0 ]]; then
+              res=1
+           else
+              if [[ ${res} != 0 ]]; then 
+                 res=2
+                 continue
+              fi
+              res=0
+           fi
+        done
+    elif [[ ${property} == 'uptime' ]]; then
+        app_exec=`jq -r '.exec' ${json} 2>/dev/null`
+        pid=`sudo jps -l 2>/dev/null | grep "${app_exec}" | awk '{print $1}'`
+	if [[ -n ${pid} ]]; then
+	    res=`sudo ps -p ${pid} -o etimes -h 2>/dev/null | awk '{$1=$1};1'`
 	fi
-    elif [[ ${resource} == 'uptime' ]]; then
-	if [[ ${rcode} == 0 ]]; then
-	    res=`sudo ps -p ${pid} -o etimes -h`
-	fi
+    elif [[ ${property} == 'version' ]]; then
+       	res=`jq -r '.version' ${json} 2>/dev/null`
+    elif [[ ${property} == 'status' ]]; then
+        url=`jq -r '.monitoring.ws.url' ${json} 2>/dev/null`
+        if [[ -n ${url/null/} ]]; then
+	   rval=`curl --insecure -s ${url} -o /dev/null -w "%{http_code}\n" 2>/dev/null`
+           rcode="${?}"
+           if [[ ${rcode} == 0 ]]; then
+	      valid_codes=`jq -r '.monitoring.ws.codes|@sh' ${json} 2>/dev/null`
+              for code in ${valid_codes[@]:-200}; do
+                 if [[ ${code} == ${rval} ]]; then
+                    res=1
+                    break
+                 fi
+              done
+           fi
+        else
+           app_exec=`jq -r '.exec' ${json} 2>/dev/null`
+           pid=`sudo jps -l 2>/dev/null | grep "${app_exec}" | awk '{print $1}'`
+	   if [[ -n ${pid} ]]; then
+              res=1           
+           fi
+        fi
     fi
-    echo ${res:-0}
+    echo "${res:-0}"
     return 0
 }
 
@@ -159,7 +214,7 @@ else
 	rval=$( get_stats ${SECTION} ${ARGS[*]} )
 	rcode="${?}"
     fi
-    echo ${rval:-0} | sed "s/null/0/g"
+    echo "${rval:-0}" | sed "s/null/0/g"
 fi
 
 exit ${rcode}
